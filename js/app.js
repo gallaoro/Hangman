830487f23cf1d794eaf301f9e90d4e6d34e11e74
lellefood <gabriele.pallaoro@gmail.com>
2014-11-14 19:25:28 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,7 +12,63 @@ $(document).ready(function () {
 var secretWord, maskedWord,
         foundarray = [false, false, false, false, false, false, false, false, false, false, false,false,false,false,false],
         ntry = 0, lettera, vittoria = false;
-var WORDS = ["abbaino", "cagnolino","coccodrillo"];
+var WORDS = ["abbaino", "cagnolino","coccodrillo","rubino","programmazione","tariffazione","fascia","immacolato","aiuole","casstello","zattera","caleidoscopio","terrestre","camaleonte","serbatoio","catarifrangente"];
+var start = function () {
+secretWord = WORDS[Math.floor((Math.random() * WORDS.length))];
+maskedWord = "";
+for (var i = 0; i < secretWord.length; i++) {
+maskedWord += " _";
+}
+$("#secret-word").html(maskedWord);
+};
+var controllalettera = function (lettera) {
+var trovata = false;
+for (var i = 0; i < secretWord.length; i++) {
+if (secretWord.charAt(i) === lettera) {
+trovata = true;
+foundarray[i] = true;
+sostituisciconlettera();
+vittoria = controllavittoria();
+if (vittoria) {
+$("#secret-word").html("Hai vinto! Complimenti, ricarica per rigiocare!");
+$("#secret-word").after("<div>La parola era "+secretWord+"</div>");
+$(".btn-try").addClass("invisible");
+$(".input").addClass("invisible");
+}
+}
+}
+if (!trovata) {
+if (ntry > 4) {
+$(".img").attr("src", "img/impiccato6.png");
+$("#secret-word").html("Hai perso! Ricarica per riprovare!");
+$(".btn-try").addClass("invisible");
+$(".input").addClass("invisible");
+} else {
+ntry++;
+$(".img").attr("src", "img/impiccato" + ntry + ".png");
+}
+}
+};
+var sostituisciconlettera = function () {
+var newmasked = "";
+for (var i = 0; i < secretWord.length; i++) {
+if (foundarray[i] === true) {
+newmasked += " " + secretWord.charAt(i);
+} else {
+newmasked += " _";
+}
+}
+$("#secret-word").html(newmasked);
+};
+var controllavittoria = function () {
+for (var i = 0; i < secretWord.length-1; i++) {
+if (foundarray[i] === false) {
+return false;
+}
+}
+return true;
+};
+
 
 var start = function () {
     secretWord = WORDS[Math.floor((Math.random() * WORDS.length))];
