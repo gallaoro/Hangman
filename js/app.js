2836b05db35fcc5c615a497a0575a6b2ecf0455c
lellefood <gabriele.pallaoro@gmail.com>
2014-11-16 17:20:15 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,66 +8,76 @@ $(document).ready(function () {
         $(".input").focus();
     });
 
+    $(document).on("keydown", function (event) {
+        if (event.which === 13) {
+            event.preventDefault();
+            lettera = $(".input").val();
+            controllalettera(lettera);
+            $(".input").val("");
+            $(".input").focus();
+        }
+    });
+
 });
 
 var secretWord, maskedWord,
-        foundarray = [false, false, false, false, false, false, false, false, false, false, false,false,false,false,false],
+        foundarray = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
         ntry = 0, lettera, vittoria = false;
-var WORDS = ["abbaino", "cagnolino","coccodrillo","rubino","programmazione","tariffazione","fascia","immacolato","aiuole","casstello","zattera","caleidoscopio","terrestre","camaleonte","serbatoio","catarifrangente"];
+var WORDS = ["abbaino", "cagnolino", "coccodrillo", "rubino", "programmazione", "tariffazione", "fascia", "immacolato", "aiuole", "casstello", "zattera", "caleidoscopio", "terrestre", "camaleonte", "serbatoio", "catarifrangente"];
 var start = function () {
-secretWord = WORDS[Math.floor((Math.random() * WORDS.length))];
-maskedWord = "";
-for (var i = 0; i < secretWord.length; i++) {
-maskedWord += " _";
-}
-$("#secret-word").html(maskedWord);
+    secretWord = WORDS[Math.floor((Math.random() * WORDS.length))];
+    maskedWord = "";
+    for (var i = 0; i < secretWord.length; i++) {
+        maskedWord += " _";
+    }
+    $("#secret-word").html(maskedWord);
 };
 var controllalettera = function (lettera) {
-var trovata = false;
-for (var i = 0; i < secretWord.length; i++) {
-if (secretWord.charAt(i) === lettera) {
-trovata = true;
-foundarray[i] = true;
-sostituisciconlettera();
-vittoria = controllavittoria();
-if (vittoria) {
-$("#secret-word").html("Hai vinto! Complimenti, ricarica per rigiocare!");
-$("#secret-word").after("<div>La parola era "+secretWord+"</div>");
-$(".btn-try").addClass("invisible");
-$(".input").addClass("invisible");
-}
-}
-}
-if (!trovata) {
-if (ntry > 4) {
-$(".img").attr("src", "img/impiccato6.png");
-$("#secret-word").html("Hai perso! Ricarica per riprovare!");
-$(".btn-try").addClass("invisible");
-$(".input").addClass("invisible");
-} else {
-ntry++;
-$(".img").attr("src", "img/impiccato" + ntry + ".png");
-}
-}
+    var trovata = false;
+    for (var i = 0; i < secretWord.length; i++) {
+        if (secretWord.charAt(i) === lettera) {
+            trovata = true;
+            foundarray[i] = true;
+            sostituisciconlettera();
+            vittoria = controllavittoria();
+            if (vittoria) {
+                $("#secret-word").html("Hai vinto! Complimenti, ricarica per rigiocare!");
+                $("#secret-word").after("<div>La parola era " + secretWord + "</div>");
+                $(".btn-try").addClass("invisible");
+                $(".input").addClass("invisible");
+            }
+        }
+    }
+    if (!trovata) {
+        if (ntry > 4) {
+            $(".img").attr("src", "img/impiccato6.png");
+            $("#secret-word").html("Hai perso! Ricarica per riprovare!");
+            $(".btn-try").addClass("invisible");
+            $(".input").addClass("invisible");
+        } else {
+            ntry++;
+            $(".img").attr("src", "img/impiccato" + ntry + ".png");
+        }
+    }
 };
 var sostituisciconlettera = function () {
-var newmasked = "";
-for (var i = 0; i < secretWord.length; i++) {
-if (foundarray[i] === true) {
-newmasked += " " + secretWord.charAt(i);
-} else {
-newmasked += " _";
-}
-}
-$("#secret-word").html(newmasked);
+    var newmasked = "";
+    for (var i = 0; i < secretWord.length; i++) {
+        if (foundarray[i] === true) {
+            newmasked += " " + secretWord.charAt(i);
+        } else {
+            newmasked += " _";
+        }
+    }
+    $("#secret-word").html(newmasked);
 };
 var controllavittoria = function () {
-for (var i = 0; i < secretWord.length-1; i++) {
-if (foundarray[i] === false) {
-return false;
-}
-}
-return true;
+    for (var i = 0; i < secretWord.length - 1; i++) {
+        if (foundarray[i] === false) {
+            return false;
+        }
+    }
+    return true;
 };
 
 
@@ -90,7 +100,7 @@ var controllalettera = function (lettera) {
             vittoria = controllavittoria();
             if (vittoria) {
                 $("#secret-word").html("Hai vinto! Complimenti, ricarica per rigiocare!");
-                $("#secret-word").after("<div>La parola era "+secretWord+"</div>");
+                $("#secret-word").after("<div>La parola era " + secretWord + "</div>");
                 $(".btn-try").addClass("invisible");
                 $(".input").addClass("invisible");
             }
@@ -121,7 +131,7 @@ var sostituisciconlettera = function () {
     $("#secret-word").html(newmasked);
 };
 var controllavittoria = function () {
-    for (var i = 0; i < secretWord.length-1; i++) {
+    for (var i = 0; i < secretWord.length - 1; i++) {
         if (foundarray[i] === false) {
             return false;
         }
